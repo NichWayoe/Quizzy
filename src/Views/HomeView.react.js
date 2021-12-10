d0317c3eed45a06603abc26a65e28413ab2dd8e9
Aashish Adhikari <a.adhikari6960@gmail.com>
2021-12-09 21:23:45 -0500

    login UI changed

--- src/Views/HomeView.react.js
+++ src/Views/HomeView.react.js
@@ -1,11 +1,13 @@
 import {MyButton,PostCard} from "../Components/styledComponents"
 import HomeCard from "../Components/HomeCard";
 import QuizCategory from "../Model/QuizCategory.react";
+
 function HomeView(props) {
     const {categoryCards} = props
 return (
     <>
     <div>Explore</div>
+    
     {categoryCards}
     </>
     
