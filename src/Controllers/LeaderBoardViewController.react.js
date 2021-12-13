d90cf6a7e7d98b786c367c3701fa29a73a8ed89f
Aashish Adhikari <a.adhikari6960@gmail.com>
2021-12-12 21:52:55 -0500

    changed views

--- src/Controllers/LeaderBoardViewController.react.js
+++
@@ -1,5 +0,0 @@
-import LeaderBoardView from '../Views/LeaderBoardView.react'
- export default function LeaderBoardViewController(){
-     return <LeaderBoardView></LeaderBoardView>
-     
- }
--- src/Views/LeaderBoardView.react.js
+++
@@ -1,4 +0,0 @@
-export default function LeaderBoardView() {
-    return <div>LeaderBoard</div>
-
-}
