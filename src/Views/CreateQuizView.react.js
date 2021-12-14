c7bfe769ce0fe7bb12fe1e4bf3684a502867624d
Aashish Adhikari <a.adhikari6960@gmail.com>
2021-12-14 11:38:49 -0500

    UI for create quiz

--- src/Views/CreateQuizView.react.js
+++ src/Views/CreateQuizView.react.js
@@ -1,4 +1,24 @@
+import { FormControl, TextField, Grid, Button } from '@mui/material';
+
 export default function CreateQuizView() {
-    return <div>Create Quiz</div>
+    return <div>
+
+        <FormControl >
+            <Grid container spacing={5} sx={{marginTop:10, padding:2, height:"50%", marginLeft:40, borderRadius:"10%"}}>
+                <Grid container item xs={6} direction="column">
+                    <TextField variant="filled" placeholder="First Name" sx={{padding:1}}/>
+                    <TextField variant="filled" placeholder="Category" sx={{padding:1}}/>
+                    <TextField variant="filled" placeholder="Description" sx={{padding:1}}/>
+
+                    </Grid> 
+                <Grid container item xs={6} direction="column">
+                <TextField variant="filled" placeholder="Last Name" sx={{padding:1}}/>
+                <TextField variant="filled" placeholder="Allowed Time" sx={{padding:1}}/>
+                <TextField variant="filled" placeholder="Title" sx={{padding:1}}/>
+                <Button variant="outlined" sx={{padding:1, marginTop:10}}> Next</Button>
+                </Grid>
+            </Grid>
+        </FormControl>
+    </div>
 
 }
